0bfa9b3e08460a7d090db9d34795f2546257b908
Raphael.work <156293692+IgarKAMJUT@users.noreply.github.com>
2026-04-16 22:45:35 +0700

    Add files via upload

---
+++ db_spp.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2026 at 05:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_spp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_spp`
+--
+
+CREATE TABLE `transaksi_spp` (
+  `id` int(11) NOT NULL,
+  `nisn` varchar(20) NOT NULL,
+  `nama_siswa` varchar(100) NOT NULL,
+  `bulan_bayar` varchar(50) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `bukti_bayar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaksi_spp`
+--
+ALTER TABLE `transaksi_spp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaksi_spp`
+--
+ALTER TABLE `transaksi_spp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
